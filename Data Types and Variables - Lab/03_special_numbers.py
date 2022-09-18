b97c8dbef486f1df9412892e6ba13e2039da3cbd
Petko <p.todorov1015@gmail.com>
2022-09-18 12:28:36 +0300

    done

--- Data Types and Variables - Lab/03_special_numbers.py
+++ Data Types and Variables - Lab/03_special_numbers.py
@@ -1,11 +1,12 @@
 n = int(input())
-sum_digits = 0
-for i in range(1,n+1):
-    while i > 0:
-        sum_digits += i % 10
-        i /= 10
+for i in range(1, n + 1):
+    digits = i
+    sum_digits = 0
+    while digits > 0:
+        sum_digits += digits % 10
+        digits //= 10
 
-    if sum_digits == 5 or sum_digits == 7 or sum_digits == 1:
+    if sum_digits == 5 or sum_digits == 7 or sum_digits == 11:
         print(f"{i} -> True")
     else:
         print(f"{i} -> False")
