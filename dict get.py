4ff64009ff0d6b6481e9a5aaa54be1aafd700966
petko <dsa@dsadas.asd>
2022-12-07 19:17:20 +0200

    test

---
+++ dict get.py
@@ -0,0 +1,24 @@
+# dict = {}
+#
+# dict['car'] = dict.get('car', {})
+# # dict['battery'] = dict.get('battery', {})
+# # dict['car']['lada'] = dict.get('lada', 1990)
+# # dict['car']['merc'] = dict.get('merc', 12)
+#
+#
+# if 'lada' not in dict['car']:
+#     dict['car']['lada'] = {}
+# dict['car']['lada']['color'] = dict.get('color', [2])
+# dict['car']['lada']['color'].append(3)
+# dict['car']['lada']['djanta'] = 1
+# print(dict)
+#
+#
+# # commands = input()
+# # event, *data = [int(x) if x.isdigit() else x for x in commands]
+# # print(type(data))
+# # print(event, *data)
+#
+
+for i in range(10, 3, -2):
+    print(i)
